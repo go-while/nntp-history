b06d1fbfb90f7d6e0518d0c268efe931931cf87d
go-while <go-while@users.noreply.github.com>
2023-10-04 10:00:15 +0200

    Create STRUCTS.go

---
+++ STRUCTS.go
@@ -0,0 +1,68 @@
+package history
+
+import (
+	"sync"
+)
+
+type HISTORY struct {
+	mux     sync.Mutex
+	cmux    sync.Mutex // sync counter
+	L1Cache L1CACHE
+	L2Cache L2CACHE
+	L3Cache L3CACHE
+	boltInitChan chan struct{}
+	boltSyncChan chan struct{}
+	Offset       int64
+	HF           string // = "history/history.dat"
+	HF_hash      string // = "history/history.Hash"
+	WriterChan   chan *HistoryObject
+	IndexChan    chan *HistoryIndex
+	IndexChans   [16]chan *HistoryIndex
+	BatchLocks   map[string]map[string]chan struct{}
+	charsMap     map[string]int
+	useHashDB    bool
+	keyalgo      int
+	keylen       int
+	win          bool
+	Counter      map[string]uint64
+	BatchQueues  *BQ
+}
+
+type BQ struct {
+	mux    sync.Mutex
+	Maps   map[string]map[string]chan *BatchOffset
+	Booted chan struct{}
+}
+
+type HistoryObject struct {
+	MessageIDHash *string
+	StorageToken  *string // "F" = flatstorage | "M" = mongodb | "X" = deleted
+	Char          *string
+	Arrival       int64
+	Expires       int64
+	Date          int64
+	ResponseChan  chan int // receives a 0,1,2 :: pass|duplicate|retrylater
+}
+
+/* builds the history.dat header */
+type HistorySettings struct {
+	KeyAlgo int
+	KeyLen  int
+}
+
+type HistoryIndex struct {
+	Hash         *string
+	Char         *string
+	Offset       int64
+	IndexRetChan chan int
+}
+
+type BatchOffset struct {
+	bucket            string
+	key               string
+	gobEncodedOffsets []byte
+}
+
+type GOBOFFSETS struct {
+	Offsets []int64
+}
