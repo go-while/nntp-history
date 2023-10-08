b74e38abd5df963e94747e3afa2ca42d60b8ad61
go-while <go-while@users.noreply.github.com>
2023-10-08 12:21:05 +0200

    Update build.sh

--- test/build.sh
+++ test/build.sh
@@ -4,6 +4,7 @@ export GOPATH=$(pwd)
 export GO111MODULE=auto
 go build nntp-history-test.go
 RET=$?
+test $RET -eq 0 && ./fmt.sh && cp -v nntp-history-test test2/
 echo $(date)
 test $RET -gt 0 && echo "BUILD FAILED! RET=$RET" || echo "BUILD OK!"
 exit $RET
