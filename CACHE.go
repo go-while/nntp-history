25717e8c20c9a1eb59458dc9ecab011f869930cc
go-while <go-while@users.noreply.github.com>
2023-10-11 20:10:16 +0200

    Update CACHE.go

--- CACHE.go
+++ CACHE.go
@@ -28,7 +28,7 @@ var (
 
 func (his *HISTORY) PrintCacheStats() {
 	// L1CACHE
-	//his.L1Cache.mux.Lock()
+
 	//statsKeys := []string{"Count_Get","Count_GetMiss","Count_Locked","Count_Set","Count_Delete","Count_BatchD","Count_Growup","Count_Shrink"}
 	l1cachesize := 0
 	l1mapsize := 0
@@ -51,10 +51,8 @@ func (his *HISTORY) PrintCacheStats() {
 		l1medium = l1cachesize / 16
 	}
 	log.Printf("L1: [fex=%d/set:%d] [get=%d/mis=%d] [del:%d/bat:%d] [g/s:%d/%d] cached:%d (~%d/char)", l1map["Count_FlagEx"], l1map["Count_Set"], l1map["Count_Get"], l1map["Count_GetMiss"], l1map["Count_Delete"], l1map["Count_BatchD"], l1map["Count_Growup"], l1map["Count_Shrink"], l1cachesize, l1medium)
-	//his.L1Cache.mux.Unlock()
 
 	// L2CACHE
-	//his.L2Cache.mux.Lock()
 	l2cachesize := 0
 	l2mapsize := 0
 	l2medium := 0
@@ -75,10 +73,8 @@ func (his *HISTORY) PrintCacheStats() {
 		l2medium = l2cachesize / 16
 	}
 	log.Printf("L2: [fex=%d/set:%d] [get=%d/mis=%d] [del:%d/bat:%d] [g/s:%d/%d] cached:%d (~%d/char)", l2map["Count_FlagEx"], l2map["Count_Set"], l2map["Count_Get"], l2map["Count_GetMiss"], l2map["Count_Delete"], l2map["Count_BatchD"], l2map["Count_Growup"], l2map["Count_Shrink"], l2cachesize, l2medium)
-	//his.L2Cache.mux.Unlock()
 
 	// L3CACHE
-	//his.L3Cache.mux.Lock()
 	l3cachesize := 0
 	l3mapsize := 0
 	l3medium := 0
@@ -99,7 +95,6 @@ func (his *HISTORY) PrintCacheStats() {
 		l3medium = l3cachesize / 16
 	}
 	log.Printf("L3: [fex=%d/set:%d] [get=%d/mis=%d] [del:%d/bat:%d] [g/s:%d/%d] cached:%d (~%d/char)", l3map["Count_FlagEx"], l3map["Count_Set"], l3map["Count_Get"], l3map["Count_GetMiss"], l3map["Count_Delete"], l3map["Count_BatchD"], l3map["Count_Growup"], l3map["Count_Shrink"], l3cachesize, l3medium)
-	//his.L3Cache.mux.Unlock()
 } // end func PrintCacheStats
 
 // gets called in BBATCH.go:boltBucketPutBatch() after boltTX
