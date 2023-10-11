bc08b825ba6fdc238f6b70c23e83f555db97a67c
go-while <go-while@users.noreply.github.com>
2023-10-11 20:43:52 +0200

    Update L3CACHE.go

--- L3CACHE.go
+++ L3CACHE.go
@@ -167,8 +167,8 @@ forever:
 
 func (l3 *L3CACHE) shrinkMapIfNeeded(char string, maplen int, oldmax int) bool {
 	shrinkmin := L3InitSize
-	thresholdFactor := 20 // shrink if percentage used of cache is lower than N
-	threshold := int(oldmax / 100 * thresholdFactor)
+	// shrink if percentage of cache usage is lower than DefaultThresholdFactor
+	threshold := int(float64(oldmax) / 100 * DefaultThresholdFactor)
 	if maplen > threshold {
 		return true
 	}
