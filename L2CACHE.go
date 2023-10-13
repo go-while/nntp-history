0ecfefa09fde37e6ebdfc8b745411064383b5d6c
go-while <go-while@users.noreply.github.com>
2023-10-13 03:25:50 +0200

    Update L2CACHE.go

--- L2CACHE.go
+++ L2CACHE.go
@@ -88,7 +88,6 @@ func (l2 *L2CACHE) L2Cache_Thread(char string) {
 	extends := make(map[int64]*ClearCache, 1024)
 	timer := time.NewTimer(time.Duration(l2purge) * time.Second)
 	timeout := false
-	//var didnotexist uint64
 forever:
 	for {
 		if timeout {
@@ -103,10 +102,6 @@ forever:
 			}
 		case <-timer.C:
 			timeout = true
-			//lenExt := len(extends)
-			//if didnotexist > 0 {
-			//	logf(DEBUGL2, "INFO L2 [%s] extends=%d didnotexist=%d", char, lenExt, didnotexist)
-			//}
 			start := utils.UnixTimeMilliSec()
 			now := int64(start / 1000)
 
@@ -120,45 +115,14 @@ forever:
 					}
 					l2.Caches[char].cache[offset].expires = now + L2ExtendExpires
 					l2.Counter[char]["Count_BatchD"] += 1
-					delete(extends, offset)
+					//delete(extends, offset)
 					continue getexpired
 				} else if item.expires > 0 && item.expires < now {
 					//logf(DEBUG, "L2 expire [%s] offset='%#v' item='%#v' age=%d l2exp=%d", char, offset, item, now-item.addtime, L2CacheExpires)
 					cleanup = append(cleanup, offset)
 				}
 			} // end for getexpired
-
-			/*
-				remextends:
-					// there may be some offsets left in extends but maybe already expired
-					for offset, extitem := range extends {
-						if extitem.char != char {
-							log.Printf("ERROR L2 [%s] extends extitem.char != char offset=%d extitem='%#v'", char, offset, extitem)
-							continue remextends
-						}
-						if _, exists := l2.Caches[extitem.char].cache[offset]; !exists {
-							didnotexist++
-							logf(DEBUG, "WARN L2 [%s] extends !exists l2.Caches[extitem.char=%s].cache[offset=%d] extitem='%#v' didnotexist=%d", char, extitem.char, offset, extitem, didnotexist)
-							delete(extends, offset)
-							continue remextends
-						}
-						if l2.Caches[char].cache[offset].hash == "" {
-							log.Printf("ERROR L2 [%s] extends empty l2.Caches[extitem.char=%s].cache[offset=%d].hash extitem='%#v'", char, extitem.char, offset, extitem)
-							delete(extends, offset)
-							continue remextends
-						}
-						if l2.Caches[char].cache[offset].hash != extitem.hash {
-							log.Printf("ERROR L2 [%s] extends invalid hash=%s extitem='%#v'", char, offset, l2.Caches[char].cache[offset].hash, extitem)
-							delete(extends, offset)
-							continue remextends
-						}
-						l2.Caches[char].cache[offset].expires = now + L2ExtendExpires
-						//l2.Caches[char].cache[offset] = &L2ITEM{hash: item.hash, expires: now + L2ExtendExpires}
-						delete(extends, offset)
-					} // end for remextends
-			*/
 			maplen := len(l2.Caches[char].cache)
-			//oldmax := l2.mapsizes[char].maxmapsize
 			l2.muxers[char].mux.Unlock()
 			clear(extends)
 			if len(cleanup) > 0 {
@@ -168,81 +132,17 @@ forever:
 					delete(l2.Caches[char].cache, offset)
 					l2.Counter[char]["Count_Delete"] += 1
 				}
-				//max := l2.mapsizes[char].maxmapsize
+				max := l2.mapsizes[char].maxmapsize
 				l2.muxers[char].mux.Unlock()
-				logf(DEBUGL2, "L2Cache_Thread [%s] deleted=%d/%d", char, len(cleanup), maplen)
+				logf(DEBUGL2, "L2Cache_Thread [%s] deleted=%d/%d max=%d", char, len(cleanup), maplen, max)
 				cleanup = nil
 			}
-			//if lastshrink < now-DefaultTryShrinkEvery {
-			//	l2.shrinkMapIfNeeded(char, maplen, oldmax)
-			//	lastshrink = now
-			//}
-			//if len(extends) != 0 {
-			//	//log.Printf("ERROR L2 [%s] extends=%d='%#v' != 0", char, len(extends), extends)
-			//	log.Printf("ERROR L2 [%s] extends=%d != 0", char, len(extends))
-			//} else {
-			/*
-				//clear(extends)
-				//extends = nil
-				if int64(lenExt) >= mapsize {
-					mapsize = int64(lenExt) * 3
-					logf(DEBUGL2, "INFO L2 [%s] grow extends=%d/%d didnotexist=%d", char, lenExt, mapsize, didnotexist)
-				}
-				extends = make(map[int64]*ClearCache, mapsize)
-			*/
-			//}
 			logf(DEBUGL2, "L2Cache_Thread [%s] (took %d ms)", char, utils.UnixTimeMilliSec()-start)
 			continue forever
 		} // end select
 	} // end for
 
 } //end func L2Cache_Thread
-
-func (l2 *L2CACHE) shrinkMapIfNeeded(char string, maplen int, oldmax int) bool {
-	return true
-
-	shrinkmin := L2InitSize
-	// shrink if percentage of cache usage is lower than DefaultThresholdFactor
-	threshold := int(float64(oldmax) / 100 * DefaultThresholdFactor)
-	if maplen > threshold {
-		return true
-	}
-	thresmax := threshold * 2
-	logf(DEBUGL2, "L2 [%s] PRE-SHRINK maplen=%d threshold=%d oldmax=%d thresmax=%d", char, maplen, threshold, oldmax, thresmax)
-	if maplen < threshold && oldmax >= thresmax && thresmax > shrinkmin {
-		newmax := threshold * 4
-		if newmax < shrinkmin {
-			newmax = shrinkmin
-		} else if oldmax == shrinkmin && newmax == shrinkmin {
-			// dont shrink lower than this
-			return true
-		}
-		return l2.shrinkMap(char, newmax, maplen)
-	}
-	return false
-} // end func shrinkMapIfNeeded
-
-func (l2 *L2CACHE) shrinkMap(char string, newmax int, maplen int) bool {
-	if char == "" {
-		log.Printf("ERROR L2CACHE shrinkMap char=nil")
-		return false
-	}
-	newmap := make(map[int64]*L2ITEM, newmax)
-	l2.muxers[char].mux.Lock()
-	if maplen > 0 {
-		for k, v := range l2.Caches[char].cache {
-			newmap[k] = v
-		}
-	}
-	//clear(l2.Caches[char].cache)
-	//l2.Caches[char].cache = nil
-	l2.Caches[char].cache = newmap
-	l2.mapsizes[char].maxmapsize = newmax
-	l2.Counter[char]["Count_Shrink"] += 1
-	l2.muxers[char].mux.Unlock()
-	logf(DBG_CGS, "L2Cache_Thread [%s] shrink size to %d maplen=%d", char, newmax, maplen)
-	return true
-} // end func shrinkMap
 
 // The SetOffsetHash method sets a cache item in the L2 cache using an offset as the key and a hash as the value.
 // It also dynamically grows the cache when necessary.
@@ -255,21 +155,6 @@ func (l2 *L2CACHE) SetOffsetHash(offset int64, hash string, flagexpires bool) {
 	//start := utils.UnixTimeMilliSec()
 	l2.muxers[char].mux.Lock()
 	defer l2.muxers[char].mux.Unlock()
-
-	/*
-		if len(l2.Caches[char].cache) >= int(l2.mapsizes[char].maxmapsize/100*98) { // grow map
-			newmax := l2.mapsizes[char].maxmapsize * 4
-			newmap := make(map[int64]*L2ITEM, newmax)
-			for k, v := range l2.Caches[char].cache {
-				newmap[k] = v
-			}
-			//clear(l2.Caches[char].cache)
-			//l2.Caches[char].cache = nil
-			l2.Caches[char].cache = newmap
-			l2.mapsizes[char].maxmapsize = newmax
-			l2.Counter[char]["Count_Growup"] += 1
-			logf(DBG_CGS, "L2CACHE [%s] grow newmap=%d/%d (took %d ms)", char, len(newmap), newmax, utils.UnixTimeMilliSec()-start)
-		}*/
 
 	expires := NoExpiresVal
 	now := utils.UnixTimeSec()
@@ -311,22 +196,6 @@ func (l2 *L2CACHE) GetHashFromOffset(offset int64) (hash *string) {
 	return
 } // end func GetHashFromOffset
 
-/*
-// The Delete method deletes a cache item from the L2 cache using an offset as the key.
-func (l2 *L2CACHE) DelExtL2(offset *int64) {
-	if offset == nil || *offset <= 0 {
-		log.Printf("ERROR L2CACHEDel offset=nil")
-		return
-	}
-	char := OffsetToChar(offset)
-	l2.muxers[*char].mux.Lock()
-	if _, exists := l2.Caches[*char].cache[*offset]; exists {
-		delete(l2.Caches[*char].cache, *offset)
-	}
-	l2.muxers[*char].mux.Unlock()
-} // end func DelExtL2
-*/
-
 // The DelExtL2batch method deletes multiple cache items from the L2 cache.
 func (l2 *L2CACHE) DelExtL2batch(his *HISTORY, tmpOffset []*ClearCache, flagCacheDelExt int) {
 	if len(tmpOffset) == 0 {
@@ -344,8 +213,8 @@ func (l2 *L2CACHE) DelExtL2batch(his *HISTORY, tmpOffset []*ClearCache, flagCach
 				}
 				if DEBUG {
 					lench := len(l2.Extend[item.char])
-					if lench >= int(float64(his.cEvCap)*0.75) {
-						log.Printf("WARN L2 Extend[%s]chan=%d/his.cEvCap=%d near-full", item.char, lench, his.cEvCap)
+					if lench >= his.cEvCap/2 {
+						log.Printf("WARN L2 Extend[%s]chan=%d/his.cEvCap=%d half-full", item.char, lench, his.cEvCap)
 					}
 				}
 				//char := l2.OffsetToChar(item.offset)
@@ -379,7 +248,6 @@ func (l2 *L2CACHE) DelExtL2batch(his *HISTORY, tmpOffset []*ClearCache, flagCach
 			l2.muxers[item.char].mux.Unlock()
 		}
 	}
-
 } // end func DelExtL2batch
 
 func (l2 *L2CACHE) OffsetToChar(offset int64) string {
