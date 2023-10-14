894a1a17794f2a31fe3bdf2b9a1829b1a9f5c637
go-while <go-while@users.noreply.github.com>
2023-10-14 16:09:15 +0200

    Update fmt.sh

--- test/fmt.sh
+++ test/fmt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 PATH="$PATH:/usr/local/go/bin"
+echo "FMT"
 go fmt *.go
 cd ../ || exit 1
 go fmt *.go
 RET=$?
-test $RET -gt 0 && echo "go fmt failed!"
+test $RET -gt 0 && echo "go fmt failed!" && exit $RET
 exit $RET
