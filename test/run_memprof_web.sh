2ff94edd475020cc1d35d986ab473c3146a4cee0
go-while <go-while@users.noreply.github.com>
2023-10-18 14:10:59 +0200

    Update run_memprof_web.sh

--- test/run_memprof_web.sh
+++ test/run_memprof_web.sh
@@ -4,7 +4,7 @@ export GOPATH=$(pwd)
 export GO111MODULE=auto
 export GOEXPERIMENT=arenas
 #go tool pprof cpu.pprof.webgrab http://127.0.0.1:1234/debug/pprof/profile
-file=cpu.pprof.out
+file=mem.pprof.out
 test ! -z "$1" && file="$1"
 ls -lh "$file"
 go tool pprof -http=:17172 "$file"
