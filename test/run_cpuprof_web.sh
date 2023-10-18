a12d628e9deb887cf43df2a8fb53c770b17bcea6
go-while <go-while@users.noreply.github.com>
2023-10-18 14:10:35 +0200

    Update run_cpuprof_web.sh

--- test/run_cpuprof_web.sh
+++ test/run_cpuprof_web.sh
@@ -3,8 +3,6 @@ PATH="$PATH:/usr/local/go/bin"
 export GOPATH=$(pwd)
 export GO111MODULE=auto
 export GOEXPERIMENT=arenas
-#go test -cpuprofile=cpu.out nntp-history-test.go || exit $?
-#go run -cpuprofile=cpu.out nntp-history-test.go || exit $?
 #go tool pprof cpu.pprof.webgrab http://127.0.0.1:1234/debug/pprof/profile
 file=cpu.pprof.out
 test ! -z "$1" && file="$1"
