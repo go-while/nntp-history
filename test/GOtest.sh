7c238873532a49dbc85f3c466644ae4c6f508f4d
go-while <go-while@users.noreply.github.com>
2023-10-20 18:32:17 +0200

    Update GOtest.sh

--- test/GOtest.sh
+++ test/GOtest.sh
@@ -1,3 +1,3 @@
-./nntp-history-test -todo 50000000 -keyalgo=11 -keylen=8 -p 4 -pprofcpu=true
+./nntp-history-test -todo 100000000 -p 4 -pprofcpu=true -numcpu=4 #-DBG_ABS1=true
 
 # ./nntp-history-test -todo 50000000 -keyalgo=11 -keylen=8 -p 4 # -pprofcpu=true -pprofmem=true
