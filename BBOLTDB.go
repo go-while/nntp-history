a25a362e4714d547e08625bf7d2fd0b009427118
go-while <go-while@users.noreply.github.com>
2023-10-22 03:17:07 +0200

    random

--- BBOLTDB.go
+++ BBOLTDB.go
@@ -514,7 +514,7 @@ func (his *HISTORY) boltDB_Worker(char string, i int, indexchan chan *HistoryInd
 					continue forbatchqueue
 				} else {
 					// queue has elements: randomly flush early to get some random distribution?
-					arand, err := generateRandomInt(1, 777)
+					arand, err := generateRandomInt(1, 100)
 					if err == nil && arand == 77 {
 						logf(DEBUG2, "forbatchqueue [%s|%s] arand=%d forced=>true Q=%d median=(%d ms) lft_slice=%d sleept=%d sleepn=%d", char, bucket, arand, Q, median, len(lft_slice), sleept, sleepn)
 						forced = true
