c91bf5885f525dae96e00a7e30c25239676cbc00
go-while <go-while@users.noreply.github.com>
2023-10-24 18:58:21 +0200

    add performance test

--- test/GOtest.sh
+++ test/GOtest.sh
@@ -1,4 +1,10 @@
+# testing bbolt batch debug
 ./nntp-history-test -numcpu=4 -BoltDB_MaxBatchSize=3 -BoltDB_MaxBatchDelay=100 -BatchFlushEvery=100 -RootBUCKETSperDB=256 -todo 100000000 -p 4 -keyindex=2 -pprofcpu=true -pprofmem=true -pprof localhost:1234 -NoReplayHisDat=false # -DBG_ABS1=true -DBG_ABS2=true
 
+# 100k+ tx/sec test
+# ./nntp-history-test -numcpu=4 -BoltDB_MaxBatchSize=1024 -BoltDB_MaxBatchDelay=100 -BatchFlushEvery=5000 -RootBUCKETSperDB=256 -todo 100000000 -p 4 -keyindex=2 -pprofcpu=true -pprofmem=true -pprof localhost:1234 -NoReplayHisDat=false # -DBG_ABS1=true -DBG_ABS2=true
+
+
+# other tests
 # ./nntp-history-test -todo 100000000 -p 4 -pprofcpu=true -numcpu=4 #-DBG_ABS1=true
 # ./nntp-history-test -todo 50000000 -keyalgo=11 -keylen=8 -p 4 # -pprofcpu=true -pprofmem=true
