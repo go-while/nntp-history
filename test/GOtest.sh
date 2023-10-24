7858a57d87c5c554c17a7d7c8a2e305e4fc18d7a
go-while <go-while@users.noreply.github.com>
2023-10-24 18:35:46 +0200

    testing values

--- test/GOtest.sh
+++ test/GOtest.sh
@@ -1,4 +1,4 @@
-./nntp-history-test -numcpu=4 -BoltDB_MaxBatchSize=3 -RootBUCKETSperDB=256 -todo 100000000 -p 4 -keyindex=2 -pprofcpu=true -pprofmem=true -pprof localhost:1234 -NoReplayHisDat=false # -DBG_ABS1=true -DBG_ABS2=true
+./nntp-history-test -numcpu=4 -BoltDB_MaxBatchSize=3 -BoltDB_MaxBatchDelay=100 -BatchFlushEvery=100 -RootBUCKETSperDB=256 -todo 100000000 -p 4 -keyindex=2 -pprofcpu=true -pprofmem=true -pprof localhost:1234 -NoReplayHisDat=false # -DBG_ABS1=true -DBG_ABS2=true
 
 # ./nntp-history-test -todo 100000000 -p 4 -pprofcpu=true -numcpu=4 #-DBG_ABS1=true
 # ./nntp-history-test -todo 50000000 -keyalgo=11 -keylen=8 -p 4 # -pprofcpu=true -pprofmem=true
