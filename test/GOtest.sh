6f7ad0105165c2f03f9fad5d3ca0d0ffbdf6755c
go-while <go-while@users.noreply.github.com>
2023-10-24 19:12:56 +0200

    try with default 4K pagesize
    lower pagesize generates more pagesplits and maybe more write traffic?

--- test/GOtest.sh
+++ test/GOtest.sh
@@ -1,5 +1,5 @@
 # testing bbolt batch debug
-./nntp-history-test -numcpu=4 -BoltDB_MaxBatchSize=3 -BoltDB_MaxBatchDelay=100 -BatchFlushEvery=100 -RootBUCKETSperDB=256 -todo 100000000 -p 4 -keyindex=2 -pprofcpu=true -pprofmem=true -pprof localhost:1234 -NoReplayHisDat=false # -DBG_ABS1=true -DBG_ABS2=true
+./nntp-history-test -numcpu=4 -BoltDB_PageSize=4 -BoltDB_MaxBatchSize=3 -BoltDB_MaxBatchDelay=100 -BatchFlushEvery=100 -RootBUCKETSperDB=256 -todo 100000000 -p 4 -keyindex=2 -pprofcpu=true -pprofmem=true -pprof localhost:1234 -NoReplayHisDat=false # -DBG_ABS1=true -DBG_ABS2=true
 
 # 100k+ tx/sec test
 # ./nntp-history-test -numcpu=4 -BoltDB_MaxBatchSize=1024 -BoltDB_MaxBatchDelay=100 -BatchFlushEvery=5000 -RootBUCKETSperDB=256 -todo 100000000 -p 4 -keyindex=2 -pprofcpu=true -pprofmem=true -pprof localhost:1234 -NoReplayHisDat=false # -DBG_ABS1=true -DBG_ABS2=true
