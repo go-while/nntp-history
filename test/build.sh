6f9d0ba4405e242b719f326842c487a2f05925c2
go-while <go-while@users.noreply.github.com>
2024-06-10 13:14:28 +0200

    Update build.sh

--- test/build.sh
+++ test/build.sh
@@ -2,8 +2,8 @@
 echo "$0"
 PATH="$PATH:/usr/local/go/bin"
 export GOPATH=$(pwd)
-export GO111MODULE=auto
-export GOEXPERIMENT=arenas
+export GO111MODULE=off
+#export GOEXPERIMENT=arenas
 go build nntp-history-test.go
 RET=$?
 test $RET -eq 0 && ./fmt.sh && cp -v nntp-history-test test2/
