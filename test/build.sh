cfd1bce862d2dd8e6ec7b0a37a710b0082deda2f
go-while <go-while@users.noreply.github.com>
2024-07-07 13:14:16 +0200

    Update build.sh

--- test/build.sh
+++ test/build.sh
@@ -6,7 +6,7 @@ export GO111MODULE=auto
 #export GOEXPERIMENT=arenas
 go build nntp-history-test.go
 RET=$?
-test $RET -eq 0 && ./fmt.sh && cp -v nntp-history-test test2/
+test $RET -eq 0 && cp -v nntp-history-test test2/
 echo $(date)
 test $RET -gt 0 && echo "BUILD FAILED! RET=$RET" || echo "BUILD OK!"
 rsync -va clear.sh nntp-history-test GOtest.sh root@spool1:/home/nntp-history/
