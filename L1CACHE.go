26a9d9713f99a3220966eb3f33d3fde4d2d5fc26
go-while <go-while@users.noreply.github.com>
2025-06-14 04:21:55 +0100

    Update L1CACHE.go

--- L1CACHE.go
+++ L1CACHE.go
@@ -87,7 +87,7 @@ func (l1 *L1CACHE) BootL1Cache(his *HISTORY) {
 		l1.Caches[char] = &L1CACHEMAP{cache: make(map[string]*L1ITEM, L1InitSize)}
 		l1.Extend[char] = &L1ECH{ch: make(chan *L1PQItem, his.cEvCap)}
 		l1.Muxers[char] = &L1MUXER{mux: sync.Mutex{}}
-		l1.Counter[char] = &CCC{Counter: make(map[string./go-g]uint64)}
+		l1.Counter[char] = &CCC{Counter: make(map[string]uint64)}
 		l1.pqQueue[char] = &L1pqQ{mux: sync.Mutex{}, que: &L1PQ{}, pqC: make(chan struct{}, 1)}
 	}
 	time.Sleep(time.Millisecond)
